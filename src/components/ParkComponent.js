e5a9bf7ce4a70ef90e4c03d4cf78093538b953f3
Angel <ansahamano@gmail.com>
2021-08-23 14:02:47 +0200

    Destructured data

--- src/components/ParkComponent.js
+++ src/components/ParkComponent.js
@@ -4,18 +4,18 @@ import { useSelector } from "react-redux";
 const ParkComponent = () => {
   const parks = useSelector((state) => state.allParks.parks);
   const renderList = parks.map((park) => {
-    const { id, name, category } = park;
+    const { id, images, name, category, creationDate } = park;
     return (
       <div className="four column wide" key="id">
         <div className="ui link cards">
           <div className="card">
             <div className="image">
-              <img src={""} alt={name} />
+              <img src={images} alt={name} />
             </div>
             <div className="content">
               <div className="header">{name}</div>
               <div className="meta name">{category}</div>
-              <div className="meta ratings">{name}</div>
+              <div className="meta ratings">{creationDate}</div>
             </div>
           </div>
         </div>
--- src/redux/reducers/parkReducer.js
+++ src/redux/reducers/parkReducer.js
@@ -13,4 +13,14 @@ export const parkReducer = (state = intialState, { type, payload }) => {
   }
 };
 
-
+// export const selectedProductsReducer = (state = {}, { type, payload }) => {
+//   console.log(type);
+//   switch (type) {
+//     case ActionTypes.SELECTED_PRODUCT:
+//       return { ...state, ...payload };
+//     case ActionTypes.REMOVE_SELECTED_PRODUCT:
+//       return {};
+//     default:
+//       return state;
+//   }
+// };
