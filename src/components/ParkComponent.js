8f02dbeee1dc4a8f3f9ad4ece319d4972b491055
Angel <ansahamano@gmail.com>
2021-08-23 16:12:30 +0200

    Added dinamic link to a website

--- src/components/ParkComponent.js
+++ src/components/ParkComponent.js
@@ -5,20 +5,42 @@ import { useSelector } from "react-redux";
 const ParkComponent = () => {
   const parks = useSelector((state) => state.allParks.parks);
   const renderList = parks.map((park) => {
-    const { id, images, name, category, creationDate } = park;
+    const {
+      id,
+      images,
+      name,
+      category,
+      creationDate,
+      postcode,
+      city,
+      country,
+    } = park;
     return (
       <div className="ui grid columns={3} divided" key="id">
         <div className="ui link cards">
           <div className="card">
             <div className="image">
-              <img src={images} alt={name} />
+              <img
+                src={
+                  images.length
+                    ? images
+                    : "https://dogorama.app/wp-content/uploads/2021/02/placeholder_park_meadow_thumbnail.png"
+                }
+                alt={name}
+              />
             </div>
             <div className="content">
               <div className="header">{name}</div>
               <div className="meta rating">{category}</div>
               <div className="meta">{creationDate}</div>
             </div>
-            <button className="ui green button">Go to Park</button>
+            <a
+              href={`https://dogorama.app/de-${country}/auslaufgebiete/${postcode}_${city}/${name}-${id}/`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <button className="ui green button">Go to Park</button>
+            </a>
           </div>
         </div>
       </div>
